ca123f40728e62753ed839b04f624a74c41b353c
Jacob Blankenburg <jblankenburg2015@aol.com>
2016-03-28 10:32:36 -0500

    Fixed Survey, added html

--- TheCookieJar/Models/Customer.cs
+++ TheCookieJar/Models/Customer.cs
@@ -7,14 +7,5 @@ namespace TheCookieJar.Models
 {
     public class Customer
     {
-        public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string StreetAddress { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int Zip { get; set; }
-        public int BoxPreference { get; set; }
-        public decimal BillingAmount { get; set; }
     }
 }
